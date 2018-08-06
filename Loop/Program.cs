25448640090939d8b35c616bc8dbcef933c050cf
Temeka <misstemeka1@live.com>
2018-08-05 22:58:15 -0500

    Attempting to loop & multiply 2-20

--- Loop/Program.cs
+++ Loop/Program.cs
@@ -6,7 +6,34 @@ namespace Loop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            //Instructing User to type a number
+            Console.WriteLine("Please type a number: ");
+            Console.ReadLine();
+
+
+            //Declaring Variables 
+            int x = 1;
+            int answer;
+
+            //Converting "int answer" to numerics
+            answer = int.Parse(Console.ReadLine());
+            
+            //Looping 2-20
+            for (int i = 1; i < 20; i++) //< not sure if 'i++' is needed...
+            
+            {                
+                answer = x * i; //trying to give end result to 'answer'
+                Console.WriteLine(answer); //tring to get it to print the 'answer' buuuut there is a "user' error
+            }
+            Console.ReadLine();
+
+            
+            
+            
+           
+          
         }
     }
 }
